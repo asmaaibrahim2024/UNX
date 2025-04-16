a87c04132bd78462615b99f5e4e33c0277e3a8d5
ItIsNour <ann5112001@gmail.com>
2025-04-16 15:19:51 +0200

    Create network diagram widget

--- src/components/sidebar/Sidebar.jsx
+++ src/components/sidebar/Sidebar.jsx
@@ -2,6 +2,7 @@
 import TraceWidget from "../widgets/trace/TraceWidget";
 import Find from "../widgets/find/Find";
 import Selection from "../widgets/selection/Selection";
+import NetworkDiagram from "../widgets/networkDiagram/NetworkDiagram";
 import { useDispatch, useSelector } from "react-redux";
 import "./Sidebar.scss";
 import { changeLanguage } from "../../redux/layout/layoutAction";
@@ -20,35 +21,41 @@ const Sidebar = () => {
     i18nInstance.changeLanguage(lng);
     dispatch(changeLanguage(lng));
   };
+
+
+  
+  // const utilityNetwork = useSelector((state) => state.traceReducer.utilityNetworkIntial);
+  // const view = useSelector((state) => state.mapViewReducer.intialView);
+
   return (
     <div className="sidebar">
-      <button
-        className="trace-button"
-        onClick={() => handleButtonClick("trace")}
-      >
+      <button className="trace-button" onClick={() => handleButtonClick("trace")} >
         <span className="trace-text">{t("Trace")}</span>
       </button>
-      <button
-        className="trace-button"
-        onClick={() => handleButtonClick("find")}
-      >
+
+      <button className="find-button" onClick={() => handleButtonClick("find")}  >
         <span className="trace-text">{t("Find")}</span>
       </button>
-      <button
-        className="trace-button"
-        onClick={() => handleButtonClick("selection")}
-      >
+
+      <button className="selection-button" onClick={() => handleButtonClick("selection")} >
         <span className="trace-text">{t("Selection")}</span>
       </button>
+
+      <button className="network-diagram-button" onClick={() => handleButtonClick("network-diagram")}  >
+        <span className="network-diagram">{t("Network Diagram")}</span>
+      </button>
+
       <button className="trace-button" onClick={toggleLanguage}>
         <span className="trace-text">{language === "en" ? "AR" : "EN"}</span>
       </button>
+
       <TraceWidget isVisible={activeButton === "trace"} />
       <Find isVisible={activeButton === "find"} />
       <Selection
         isVisible={activeButton === "selection"}
         setActiveButton={setActiveButton}
       />
+      <NetworkDiagram isVisible={activeButton === "network-diagram"}/>
     </div>
   );
 };
