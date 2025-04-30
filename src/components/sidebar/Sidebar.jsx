05aadc1e03870a26fb08cb4719de3a7f542936df
Ahmed hamdy ghanem <ahmedghanem99943@gmail.com>
2025-04-30 19:33:27 +0300

    selection

--- src/components/sidebar/Sidebar.jsx
+++ src/components/sidebar/Sidebar.jsx
@@ -1,20 +1,20 @@
 ﻿import { useState } from "react";
 import TraceWidget from "../widgets/trace/TraceWidget";
 import Find from "../widgets/find/Find";
+import Validate from "../widgets/validate/Validate";
 import Selection from "../widgets/selection/Selection";
 import NetworkDiagram from "../widgets/networkDiagram/NetworkDiagram";
 import { useDispatch, useSelector } from "react-redux";
 import "./Sidebar.scss";
 import { changeLanguage } from "../../redux/layout/layoutAction";
-import {useI18n} from "../../handlers/languageHandler"
-import trace from '../../style/images/trace.svg';
-import validate from '../../style/images/validate.svg';
-import selection from '../../style/images/selection.svg';
-import versions from '../../style/images/versions.svg';
-import diagrams from '../../style/images/diagrams.svg';
-import maps from '../../style/images/map-setting.svg';
-
-
+import { useI18n } from "../../handlers/languageHandler";
+import trace from "../../style/images/trace.svg";
+import validate from "../../style/images/validate.svg";
+import selection from "../../style/images/selection.svg";
+import versions from "../../style/images/versions.svg";
+import diagrams from "../../style/images/diagrams.svg";
+import maps from "../../style/images/map-setting.svg";
+import help from "../../style/images/help-circle.svg";
 
 const Sidebar = () => {
   const { t, direction, dirClass, i18nInstance } = useI18n("Sidebar");
@@ -36,70 +36,93 @@ const Sidebar = () => {
 
   return (
     <>
-  <div className="sidebar">
-    <button
-      className={`trace-button ${activeButton === "trace" ? "active" : ""}`}
-      onClick={() => handleButtonClick("trace")}
-    >
-      <img src={trace} alt="trace" />
-      <span className="trace-text">{t("Trace")}</span>
-    </button>
+      <div className="sidebar">
+        <section>
+          <button
+            className={`trace-button ${
+              activeButton === "trace" ? "active" : ""
+            }`}
+            onClick={() => handleButtonClick("trace")}
+          >
+            <img src={trace} alt="trace" />
+            <span className="trace-text">{t("Trace")}</span>
+          </button>
 
-    {/* <button
-      className={`trace-button ${activeButton === "find" ? "active" : ""}`}
-      onClick={() => handleButtonClick("find")}
-    >
-      <img src={validate} alt="validate" />
-      <span className="trace-text">{t("Find")}</span>
-    </button> */}
+          <button
+            className={`trace-button ${
+              activeButton === "validate" ? "active" : ""
+            }`}
+            onClick={() => handleButtonClick("validate")}
+          >
+            <img src={validate} alt="validate" />
+            <span className="trace-text">{t("Validate")}</span>
+          </button>
 
-    <button
-      className={`trace-button ${activeButton === "selection" ? "active" : ""}`}
-      onClick={() => handleButtonClick("selection")}
-    >
-      <img src={selection} alt="selection" />
-      <span className="trace-text">
-        {t("Selection")}
-        <span className="countSelect">12</span>
-      </span>
-    </button>
+          <button
+            className={`trace-button ${
+              activeButton === "selection" ? "active" : ""
+            }`}
+            onClick={() => handleButtonClick("selection")}
+          >
+            <img src={selection} alt="selection" />
+            <span className="trace-text">
+              {t("Selection")}
+              <span className="countSelect">12</span>
+            </span>
+          </button>
 
-    <button
-      className={`trace-button ${activeButton === "versions" ? "active" : ""}`}
-      onClick={() => handleButtonClick("versions")}
-    >
-      <img src={versions} alt="versions" />
-      <span className="trace-text">{t("versions")}</span>
-    </button>
+          <button
+            className={`trace-button ${
+              activeButton === "versions" ? "active" : ""
+            }`}
+            onClick={() => handleButtonClick("versions")}
+          >
+            <img src={versions} alt="versions" />
+            <span className="trace-text">{t("versions")}</span>
+          </button>
 
-    <button
-      className={`trace-button ${activeButton === "diagrams" ? "active" : ""}`}
-      onClick={() => handleButtonClick("diagrams")}
-    >
-      <img src={diagrams} alt="diagrams" />
-      <span className="trace-text">{t("diagrams")}</span>
-    </button>
+          <button
+            className={`trace-button ${
+              activeButton === "diagrams" ? "active" : ""
+            }`}
+            onClick={() => handleButtonClick("diagrams")}
+          >
+            <img src={diagrams} alt="diagrams" />
+            <span className="trace-text">{t("diagrams")}</span>
+          </button>
+        </section>
 
-    <button
-      className={`trace-button ${activeButton === "map" ? "active" : ""}`}
-      onClick={() => handleButtonClick("map")}
-    >
-      <img src={maps} alt="map" />
-      <span className="trace-text">{t("Map Service")}</span>
-    </button>
-  </div>
+        <section>
+          <button
+            className={`trace-button ${activeButton === "map" ? "active" : ""}`}
+            onClick={() => handleButtonClick("map")}
+          >
+            <img src={maps} alt="map" />
+            <span className="trace-text">{t("Map Service")}</span>
+          </button>
+          <button
+            className={`trace-button ${
+              activeButton === "help" ? "active" : ""
+            }`}
+            onClick={() => handleButtonClick("help")}
+          >
+            <img src={help} alt="map" />
+            <span className="trace-text">{t("Help")}</span>
+          </button>
+        </section>
+      </div>
 
-  <div className="sub-sidebar">
-    <TraceWidget isVisible={activeButton === "trace"}  setActiveButton={setActiveButton}/>
-    {/* <Find isVisible={activeButton === "find"} /> */}
-    <Selection
-      isVisible={activeButton === "selection"}
-    />
-          <NetworkDiagram isVisible={activeButton === "network-diagram"} />
-
-  </div>
-</>
-
+      <div className="sub-sidebar">
+        <TraceWidget
+          isVisible={activeButton === "trace"}
+          setActiveButton={setActiveButton}
+        />
+        {/* <Find isVisible={activeButton === "find"} /> */}
+        <Validate isVisible={activeButton === "validate"}></Validate>
+        <Selection isVisible={activeButton === "selection"} />
+        <NetworkDiagram isVisible={activeButton === "network-diagram"} />
+      </div>
+    </>
   );
 };
 export default Sidebar;
