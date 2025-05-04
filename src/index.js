cc2d7d45d83ebd547632ef26f10b11fbae09e9a1
Fatma Mahmoud <eng.fatima.moawad@gmail.com>
2025-05-04 09:22:28 +0300

    add bootstrap to project

--- src/index.js
+++ src/index.js
@@ -5,6 +5,9 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 import store from './redux/store';
 import { Provider } from 'react-redux';
+//bootstrap
+import 'bootstrap/dist/css/bootstrap.css';
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
  <Provider store={ store }>
