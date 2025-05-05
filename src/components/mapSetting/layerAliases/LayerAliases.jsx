f396b3eb5f98c3e2d67f3ecba5b046af2cf0a6c8
Fatma Mahmoud <eng.fatima.moawad@gmail.com>
2025-05-05 16:17:27 +0300

    map setting (layer aliases) component

---
+++ src/components/mapSetting/layerAliases/LayerAliases.jsx
@@ -0,0 +1,87 @@
+import { React, useState } from "react";
+import { InputText } from "primereact/inputtext";
+import { Dropdown } from "primereact/dropdown";
+import "./LayerAliases.scss";
+import { useI18n } from "../../../handlers/languageHandler";
+import reset from "../../../style/images/refresh.svg";
+
+export default function LayerAliases() {
+  const { t, direction, dirClass, i18nInstance } = useI18n("MapSetting");
+
+  const [value, setValue] = useState("");
+  const [selectedCity, setSelectedCity] = useState(null);
+  const cities = [
+    { name: "New York", code: "NY" },
+    { name: "Rome", code: "RM" },
+    { name: "London", code: "LDN" },
+    { name: "Istanbul", code: "IST" },
+    { name: "Paris", code: "PRS" },
+  ];
+
+  const items = ["Item 1", "Item 2", "Item 3", "1", "5", "555", "55555555555"];
+
+  return (
+    <div className="card border-0 rounded_0 h-100 p_x_32 p_y_16">
+      <div className="card-body">
+        <div>
+          <div className="d-flex flex-column m_b_16">
+            <label className="m_b_8">{t("Layer Name")}</label>
+            <Dropdown
+              value={selectedCity}
+              onChange={(e) => setSelectedCity(e.value)}
+              options={cities}
+              optionLabel="name"
+              placeholder={t("Select Layer Name")}
+              className="w-full md:w-14rem"
+              filter
+            />
+          </div>
+          {items.map((item, index) => (
+            <div className="row g-4" key={index}>
+              <div className="col-4">
+                <div className="d-flex flex-column m_b_16">
+                  <label className="m_b_8">
+                    {t("Field Name")} {index + 1}
+                  </label>
+                </div>
+              </div>
+              <div className="col-4">
+                <div className="d-flex flex-column m_b_16">
+                  <label className="m_b_8">
+                    {t("Alias English Name")}
+                  </label>
+                  <InputText
+                    value={value}
+                    onChange={(e) => setValue(e.target.value)}
+                    className="p-inputtext-sm"
+                  />
+                </div>
+              </div>
+              <div className="col-4">
+                <div className="d-flex flex-column m_b_16">
+                  <label className="m_b_8">
+                    {t("Alias Arabic Name")}
+                  </label>
+                  <InputText
+                    value={value}
+                    onChange={(e) => setValue(e.target.value)}
+                    className="p-inputtext-sm"
+                  />
+                </div>
+              </div>
+            </div>
+          ))}
+        </div>
+      </div>
+      <div className="card-footer bg-transparent border-0">
+        <div className="action-btns pb-2">
+          <button className="reset">
+            <img src={reset} alt="reset" />
+            {t("Reset")}
+          </button>
+          <button className="trace">{t("Connect")}</button>
+        </div>
+      </div>
+    </div>
+  );
+}
--- src/components/mapSetting/networkService/NetworkService.jsx
+++ src/components/mapSetting/networkService/NetworkService.jsx
@@ -7,30 +7,44 @@ import reset from "../../../style/images/refresh.svg";
 export default function NetworkService() {
   const { t, direction, dirClass, i18nInstance } = useI18n("MapSetting");
 
-  const [value, setValue] = useState('');
+  const [value, setValue] = useState("");
 
   return (
     <div className="card border-0 rounded_0 h-100 p_x_32 p_y_16">
       <div className="card-body">
-        <div className="d-flex flex-column m_b_16">
-          <label className="m_b_8">{t("Utility Network Service")}</label>
-          <InputText value={value} onChange={(e) => setValue(e.target.value)}
-          className="p-inputtext-sm" />
-        </div>
-        <div className="d-flex flex-column m_b_16">
-          <label className="m_b_8">{t("Diagram Service URL")}</label>
-          <InputText value={value} onChange={(e) => setValue(e.target.value)}
-          className="p-inputtext-sm" />
-        </div>
-        <div className="d-flex flex-column m_b_16">
-          <label className="m_b_8">{t("Feature Service URL")}</label>
-          <InputText value={value} onChange={(e) => setValue(e.target.value)}
-          className="p-inputtext-sm" />
-        </div>
-        <div className="d-flex flex-column m_b_16">
-          <label className="m_b_8">{t("Default basemap")}</label>
-          <InputText value={value} onChange={(e) => setValue(e.target.value)}
-          className="p-inputtext-sm" />
+        <div>
+          <div className="d-flex flex-column m_b_16">
+            <label className="m_b_8">{t("Utility Network Service")}</label>
+            <InputText
+              value={value}
+              onChange={(e) => setValue(e.target.value)}
+              className="p-inputtext-sm"
+            />
+          </div>
+          <div className="d-flex flex-column m_b_16">
+            <label className="m_b_8">{t("Diagram Service URL")}</label>
+            <InputText
+              value={value}
+              onChange={(e) => setValue(e.target.value)}
+              className="p-inputtext-sm"
+            />
+          </div>
+          <div className="d-flex flex-column m_b_16">
+            <label className="m_b_8">{t("Feature Service URL")}</label>
+            <InputText
+              value={value}
+              onChange={(e) => setValue(e.target.value)}
+              className="p-inputtext-sm"
+            />
+          </div>
+          <div className="d-flex flex-column m_b_16">
+            <label className="m_b_8">{t("Default basemap")}</label>
+            <InputText
+              value={value}
+              onChange={(e) => setValue(e.target.value)}
+              className="p-inputtext-sm"
+            />
+          </div>
         </div>
       </div>
       <div className="card-footer bg-transparent border-0">
