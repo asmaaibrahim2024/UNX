4dd730b34d4a3e5a1b5071239628c25e716e1398
Fatma Mahmoud <eng.fatima.moawad@gmail.com>
2025-05-06 13:52:52 +0300

    map setting configuration tabs

--- src/redux/mapSetting/mapSettingAction.js
+++ src/redux/mapSetting/mapSettingAction.js
@@ -6,3 +6,54 @@ export const setMapSettingVisiblity = (mapSettingVisiblity) => {
     payload: mapSettingVisiblity,
   };
 };
+
+export const setMapSettingConfigActiveButton = (activeButton) => {
+  return {
+    type: ActionTypes.SET_MAP_SETTING_ACTIVE_BUTTON,
+    payload: activeButton,
+  };
+};
+
+export const setNetworkServicesVisiblity = (networkServices) => {
+  return {
+    type: ActionTypes.SET_NETWORK_SERVICES_VISIBLITY,
+    payload: networkServices,
+  };
+};
+
+export const setLayerAliasesVisiblity = (layerAliases) => {
+  return {
+    type: ActionTypes.SET_LAYER_ALIASES_VISIBLITY,
+    payload: layerAliases,
+  };
+};
+
+export const setSearchableLayersVisiblity = (searchableLayers) => {
+  return {
+    type: ActionTypes.SET_SEARCHABLE_LAYERS_VISIBLITY,
+    payload: searchableLayers,
+  };
+};
+
+export const setPropertiesLayerFieldsVisiblity = (propertiesLayerFields) => {
+  return {
+    type: ActionTypes.SET_PROPERTIES_LAYER_FIELDS_VISIBLITY,
+    payload: propertiesLayerFields,
+  };
+};
+
+export const setResultDetailsLayerFieldsVisiblity = (resultDetailsLayerFields) => {
+  return {
+    type: ActionTypes.SET_RESULT_DETAILS_LAYER_FIELDS_VISIBLITY,
+    payload: resultDetailsLayerFields,
+  };
+};
+
+export const setIdentifyDetailsLayerFieldsVisiblity = (identifyDetailsLayerFields) => {
+  return {
+    type: ActionTypes.SET_IDENTIFY_DETAILS_LAYER_FIELDS_VISIBLITY,
+    payload: identifyDetailsLayerFields,
+  };
+};
+
+
