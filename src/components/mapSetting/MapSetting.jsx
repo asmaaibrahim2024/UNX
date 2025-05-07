078388c1a932a01197e487f2b7f4581fd2bea3bd
Fatma Mahmoud <eng.fatima.moawad@gmail.com>
2025-05-07 12:17:36 +0300

    create other components of map setting config

--- src/components/mapSetting/MapSetting.jsx
+++ src/components/mapSetting/MapSetting.jsx
@@ -4,6 +4,9 @@ import "./MapSetting.scss";
 import NetworkService from "./networkService/NetworkService";
 import LayerAliases from "./layerAliases/LayerAliases";
 import SearchableLayers from "./searchableLayers/SearchableLayers";
+import PropertiesFields from "./propertiesFields/PropertiesFields";
+import SearchResultFields from "./searchResultFields/SearchResultFields";
+import IdentifyFields from "./identifyFields/IdentifyFields";
 
 export default function MapSetting() {
 
@@ -19,9 +22,24 @@ export default function MapSetting() {
     (state) => state.mapSettingReducer.searchableLayers
   );
 
+  const propertiesLayerFields = useSelector(
+    (state) => state.mapSettingReducer.propertiesLayerFields
+  );
+
+  const resultDetailsLayerFields = useSelector(
+    (state) => state.mapSettingReducer.resultDetailsLayerFields
+  );
+
+  const identifyDetailsLayerFields = useSelector(
+    (state) => state.mapSettingReducer.identifyDetailsLayerFields
+  );
+
   return <div className="map_setting_container">
     {networkServices && <NetworkService/>}
     {layerAliases && <LayerAliases/>}
     {searchableLayers && <SearchableLayers/>}
+    {propertiesLayerFields && <PropertiesFields/>}
+    {resultDetailsLayerFields && <SearchResultFields/>}
+    {identifyDetailsLayerFields && <IdentifyFields/>}
   </div>;
 }
