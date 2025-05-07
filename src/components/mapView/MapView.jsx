b35038c10e98933d65c6b7c0a894281ac76c3180
ItIsNour <ann5112001@gmail.com>
2025-05-07 13:33:38 +0300

    Uncomment

--- src/components/mapView/MapView.jsx
+++ src/components/mapView/MapView.jsx
@@ -167,12 +167,8 @@ export default function MapView() {
         dispatch(setNetworkService(networkService));
 
         //create the utility network and dispatch to the store
-        // utilityNetwork = await createUtilityNetwork(networkService.serviceUrl);
+        utilityNetwork = await createUtilityNetwork(networkService.serviceUrl);
 
-        utilityNetwork = await createUtilityNetwork(
-          window.mapConfig.portalUrls.utilityNetworkLayerUrl
-        );
-        
         // utilityNetwork = utilityNetworkMapSettings;
 
         await utilityNetwork.load();
