bc9c23e30aac0eee8d089ccb3ff91149a089680f
MariamHassanElDeeb <mariamhassangis@gmail.com>
2025-05-11 13:48:19 +0300

    adding ui for 404

--- src/App.js
+++ src/App.js
@@ -10,6 +10,7 @@ import { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { setUserData } from "../src/redux/layout/layoutAction";
 import ProtectedRoute from "./components/layout/ProtectedPage"; // Import the wrapper
+import NotFound404 from './components/layout/NotFound404';
 
 function App() {
       const dispatch = useDispatch();
@@ -19,27 +20,27 @@ function App() {
     dir: i18n.dir(i18n.language),
   });
   //uncomment the following to activate authHandlers
-  // useEffect(() => {
-  //   debugger
-  //   // Call your auth function on app startup
-  //   AuthService.getUserByUniqueId()
-  //     .then((user) => {
-  //       dispatch(setUserData(user))
-  //       console.log("User info:", user);
-  //     })
-  //     .catch((err) => {
-  //       console.error("Failed to get user:", err);
-  //     });
-  // }, []);
+  useEffect(() => {
+    debugger
+    // Call your auth function on app startup
+    AuthService.getUserByUniqueId()
+      .then((user) => {
+        dispatch(setUserData(user))
+        console.log("User info:", user);
+      })
+      .catch((err) => {
+        console.error("Failed to get user:", err);
+      });
+  }, []);
   return (
     <I18nextProvider i18n={i18n}>
       <BrowserRouter>
   
-       <Routes>
+       {/* <Routes>
           <Route path="/*" element={<Layout />} />
-        </Routes> 
+        </Routes>  */}
         {/*uncomment the following to activate role guard*/}
-         {/* <Routes>
+         <Routes>
           <Route
             path="/*"
             element={
@@ -48,8 +49,10 @@ function App() {
               </ProtectedRoute>
             }
           />
-          <Route path="/error/500" element={<div>Error 500 - Unauthorized</div>} />
-        </Routes> */}
+            <Route path="/error/500" element={<NotFound404 />} />
+
+          {/* <Route path="/error/500" element={<div>Error 500 - Unauthorized</div>} /> */}
+        </Routes>
       </BrowserRouter>
     </I18nextProvider>
   );
---
+++ src/components/layout/NotFound404.jsx
@@ -0,0 +1,39 @@
+// src/pages/NotFound404.js
+
+import React from "react";
+import { Link } from "react-router-dom";
+ import darkError from "../../style/images/not-authorized.png";
+
+const NotFound404 = () => {
+  return (
+    <div className="text-center p-5">
+      {/* Illustration */}
+      <div className="mb-10">
+        <img
+          src={darkError}
+          className="mw-100 mh-300px theme-dark-show"
+          alt="404 Dark"
+        />
+      </div>
+
+      {/* Title */}
+      <h1 className="fw-bolder fs-2hx text-gray-900 mb-4">
+        We can't find that page
+      </h1>
+
+      {/* Text */}
+      <div className="fw-semibold fs-6 text-gray-500 mb-7">
+        Click return home to go back to the home screen.
+      </div>
+
+      {/* Link */}
+      <div className="mb-0">
+        <Link to={window.appConfig.apiServer.utilityKitURL} className="btn btn-dark">
+        Return Home
+        </Link>
+      </div>
+    </div>
+  );
+};
+
+export default NotFound404;
