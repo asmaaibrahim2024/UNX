c81fbb3d7429efcb51a57db609dfe2beab522cb8
Fatma Mahmoud <eng.fatima.moawad@gmail.com>
2025-05-22 15:13:20 +0300

    data time picker search on trace history

--- src/components/widgets/trace/traceHistory/TraceHistory.jsx
+++ src/components/widgets/trace/traceHistory/TraceHistory.jsx
@@ -1,4 +1,4 @@
-import { React, useState, useEffect } from "react";
+import { React, useState, useEffect, useRef } from "react";
 import { useSelector, useDispatch } from "react-redux";
 import { fetchTraceHistory } from "../traceHandler";
 import "./TraceHistory.scss";
@@ -12,18 +12,20 @@ import arrowdown from "../../../../style/images/cheveron-down.svg";
 import search from "../../../../style/images/search.svg";
 
 import { Accordion, AccordionTab } from "primereact/accordion";
+import { Calendar } from "primereact/calendar";
 import { IconField } from "primereact/iconfield";
 import { InputIcon } from "primereact/inputicon";
-import { InputText } from 'primereact/inputtext';
+// import { InputText } from 'primereact/inputtext';
 
 export default function TraceHistory({ setActiveTab, setActiveButton }) {
   const { t, direction } = useI18n("Trace");
   const dispatch = useDispatch();
   const [activeIndex, setActiveIndex] = useState([0]); // Initialize with first tab open, adjust as needed
+  const [datetime12h, setDateTime12h] = useState(null);
 
   // Sample data for the accordion tabs
   const items = [
-    { name: "Today", content: ["10:10:02", "09:00,00"] },
+    { name: "Today", content: ["09:00,00"] },
     { name: "Yesterday", content: ["08:09:00", "10:10:02", "09:00,00"] },
     { name: "Tuesday", content: ["09:00,00"] },
     { name: "Monday", content: ["09:09:00", "10:10:02"] },
@@ -35,6 +37,7 @@ export default function TraceHistory({ setActiveTab, setActiveButton }) {
     { name: "Last Month", content: ["09:09:00", "10:10:02"] },
     { name: "Older", content: ["09:09:00"] },
   ];
+
 
   useEffect(() => {
     console.log("Trace History component");
@@ -67,69 +70,87 @@ export default function TraceHistory({ setActiveTab, setActiveButton }) {
       </div>
       <div className="subSidebar-widgets-body trace-body">
         <div className="h-100 position-relative p-2 d-flex flex-column">
+          {/*search by date time*/}
           <div className="flex-shrink-0 mb-2">
             <IconField iconPosition="left" className="p-icon-field-custom">
-              <InputIcon><img src={search} alt="search"/></InputIcon>
-              <InputText className="p-inputtext-sm w-100" placeholder={t("search")} />
+              <InputIcon>
+                <img src={search} alt="search" />
+              </InputIcon>
+              <Calendar
+                placeholder={t("search")}
+                value={datetime12h}
+                onChange={(e) => setDateTime12h(e.value)}
+                showTime
+                hourFormat="12"
+                showButtonBar
+                style={{ width: "100%" }}
+                inputStyle={{ width: "100%" }}
+                selectionMode="single" // Ensures popup closes on date selection
+              />
             </IconField>
           </div>
-          <Accordion
-            multiple
-            activeIndex={activeIndex}
-            onTabChange={(e) => setActiveIndex(e.index)}
-            className="accordion-custom flex-fill overflow-auto p_x_4"
-          >
-            {items.map((item, index) => (
-              <AccordionTab
-                key={index}
-                header={
-                  <span className="flex align-items-center gap-2 w-full d-flex justify-content-between">
-                    <span className="font-bold white-space-nowrap">
-                      {item.name}
+          {/*history list*/}
+          {items.length > 0 ? (
+            <Accordion
+              multiple
+              activeIndex={activeIndex}
+              onTabChange={(e) => setActiveIndex(e.index)}
+              className="accordion-custom flex-fill overflow-auto p_x_4"
+            >
+              {items.map((item, index) => (
+                <AccordionTab
+                  key={index}
+                  header={
+                    <span className="flex align-items-center gap-2 w-full d-flex justify-content-between">
+                      <span className="font-bold white-space-nowrap">
+                        {item.name}
+                      </span>
+                      <img
+                        src={activeIndex.includes(index) ? arrowup : arrowdown}
+                        alt={
+                          activeIndex.includes(index) ? "collapse" : "expand"
+                        }
+                        className="cursor-pointer"
+                        height="18"
+                      />
                     </span>
-                    <img
-                      src={activeIndex.includes(index) ? arrowup : arrowdown}
-                      alt={activeIndex.includes(index) ? "collapse" : "expand"}
-                      className="cursor-pointer"
-                      height="18"
-                    />
-                  </span>
-                }
-              >
-                {/* <p>{item.content}</p> */}
-                {item.content.length > 0 && (
-                  <ul className="trace_history_list flex-fill overflow-auto p_x_4">
-                    {Array.isArray(item.content) ? (
-                      item.content.map((itemContent, iC) => (
-                        <li
-                          key={iC}
-                          className="d-flex flex-row justify-content-between rounded-1"
-                        >
-                          <span className="title">{itemContent}</span>
-                          <div className="d-flex align-items-center">
-                            <img
-                              src={trash}
-                              alt="trash"
-                              className="cursor-pointer"
-                              height="18"
-                            />
-                            <img
-                              src={edit}
-                              alt="edit"
-                              className="cursor-pointer m_l_8"
-                              height="16"
-                            />
-                          </div>
-                        </li>
-                      ))
-                    ) : (
-                      <p>{item.content}</p>
-                    )}
-                  </ul>
-                )}
-              </AccordionTab>
-            ))}
-          </Accordion>
+                  }
+                >
+                  {item.content.length > 0 ? (
+                    <ul className="trace_history_list flex-fill overflow-auto p_x_4">
+                      {Array.isArray(item.content) &&
+                        item.content.map((itemContent, iC) => (
+                          <li
+                            key={iC}
+                            className="d-flex flex-row justify-content-between rounded-1"
+                          >
+                            <span className="title">{itemContent}</span>
+                            <div className="d-flex align-items-center">
+                              <img
+                                src={trash}
+                                alt="trash"
+                                className="cursor-pointer"
+                                height="18"
+                              />
+                              <img
+                                src={edit}
+                                alt="edit"
+                                className="cursor-pointer m_l_8"
+                                height="16"
+                              />
+                            </div>
+                          </li>
+                        ))}
+                    </ul>
+                  ) : (
+                    <div className="element-item-noData">{t("No Data")}</div>
+                  )}
+                </AccordionTab>
+              ))}
+            </Accordion>
+          ) : (
+            <div className="element-item-noData">{t("No Data History")}</div>
+          )}
         </div>
       </div>
 
