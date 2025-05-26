2319f787f1188027db432f86d184eed5bdf74c08
MariamHassanElDeeb <mariamhassangis@gmail.com>
2025-05-26 15:45:08 +0300

    handled network diagram as esri

--- src/components/mapContainer/MapContainer.jsx
+++ src/components/mapContainer/MapContainer.jsx
@@ -11,14 +11,61 @@ export default function MapContainer({ setLoading }) {
   const diagramModelData = useSelector(
     (state) => state.networkDiagramReducer.diagramModelData
   );
+  
     const isDiagramLoading = useSelector(
       (state) => state.networkDiagramReducer.isDiagramLoadingIntial
     );
+      const diagramExportUrl = useSelector(
+        (state) => state.networkDiagramReducer.diagramExportUrlIntial
+      );
   useEffect(()=>{
     console.log(diagramModelData,"diagramModelData");
     
   },[diagramModelData])
-  return (
+  // return (
+  //   <Splitter className="h-100" style={{ height: "100%" }}>
+  //     <SplitterPanel
+  //       size={isNetworkDiagramSplitterVisible ? 50 : 100}
+  //       className="flex align-items-center justify-content-center"
+  //     >
+  //       <MapView setLoading={setLoading} />
+  //     </SplitterPanel>
+
+  //     {/* Always render second panel */}
+  //     <SplitterPanel
+  //       size={isNetworkDiagramSplitterVisible ? 50 : 0}
+  //       className="flex align-items-center justify-content-center"
+  //       style={{
+  //         overflow: "hidden",
+  //         display: isNetworkDiagramSplitterVisible ? "block" : "none",
+  //       }}
+  //     >
+  //             {isDiagramLoading &&(
+  //       <div className="apploader_container apploader_container_widget">
+  //         <span className="apploader"></span>
+  //       </div>
+  //     )}
+  //      {diagramModelData&&<NetworkDiagramMapView />}
+  //     </SplitterPanel>
+  //   </Splitter>
+  // );
+//  return (
+//     <>
+//       {!isNetworkDiagramSplitterVisible ? (
+//         <MapView setLoading={setLoading} />
+//       ) : (
+//         <Splitter className="h-100">
+//           <SplitterPanel className="flex align-items-center justify-content-center">
+//             <MapView setLoading={setLoading} />
+//           </SplitterPanel>
+//           <SplitterPanel className="flex align-items-center justify-content-center">
+//             <NetworkDiagramMapView setLoading={setLoading} />
+//           </SplitterPanel>
+//         </Splitter>
+//       )}
+//     </>
+//   );
+    return (
     <Splitter className="h-100" style={{ height: "100%" }}>
       <SplitterPanel
         size={isNetworkDiagramSplitterVisible ? 50 : 100}
@@ -37,11 +84,15 @@ export default function MapContainer({ setLoading }) {
         }}
       >
               {isDiagramLoading &&(
-        <div className="apploader_container apploader_container_widget">
-          <span className="apploader"></span>
+        <div className="loading-overlay">
+          {/* <div className="loading-spinner">Loading...</div> */}
+          {/* <span className="loader"></span> */}
+          <div className="apploader_container">
+            <span className="apploader"></span>
+          </div>
         </div>
       )}
-       {diagramModelData&&<NetworkDiagramMapView />}
+       {isNetworkDiagramSplitterVisible&&<NetworkDiagramMapView />}
       </SplitterPanel>
     </Splitter>
   );
